a44b40e1da517cdb9c07bc2b5bf2e01f6631ce05
Fuzznip <fuzznip@gmail.com>
2025-04-02 02:03:35 -0700

    Add ranks endpoint to retrieve all clan ranks

---
+++ endpoints/ranks.py
@@ -0,0 +1,10 @@
+from app import app, db
+from helper.helpers import ModelEncoder
+from flask import request
+from models.models import ClanRanks
+import json
+
+@app.route('/ranks', methods=['GET'])
+def get_all_ranks():
+    ranks = ClanRanks.query.order_by(ClanRanks.rank_order).all()
+    return json.dumps([rank.serialize() for rank in ranks], cls=ModelEncoder)
